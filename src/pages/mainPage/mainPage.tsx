098ee64bd01b3f55d0674a9370309311792d6ed8
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-03-01 21:04:55 +0300

    create MainPage with header

--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -1,6 +1,26 @@
+import AlertIcon from '../../components/icons/alertIcon';
+import RefreshIcon from '../../components/icons/refreshIcon';
+import styles from './mainPage.module.css';
 
 const MainPage = () => {
-    return (<></>)
+    return (
+        <div className={styles.main}>
+            <div className={styles.header}>
+                <h1 className={styles.title}>Match Tracker</h1>
+                <div className={styles.update}>
+                    <div className={styles.warning}>
+                        <AlertIcon />
+                        <p className={styles.error}>Ошибка: не удалось загрузить информацию</p>
+                    </div>
+                    <button className={styles.refresh}>
+                        <p>Обновить</p>
+                        <RefreshIcon />
+                    </button>
+                </div>
+            </div>
+            <div className={styles.matches}></div>
+        </div>
+    )
 }
 
 export default MainPage;
